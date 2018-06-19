8f36c704502ce862a9b6e2f385dd191979a9b4cf
HyunA <foren4012@gmail.com>
2018-06-19 16:05:17 +0900

    Conv_autoencoder

--- Data/Bearing_Preprocess.py
+++ Data/Bearing_Preprocess.py
@@ -14,43 +14,43 @@ merge_dir = '..\\Data\\Dataset\\Bearing\\Bearing'
 # 이미지 데이터분리 경로
 train_test_dir =  '..\\Data\\Dataset\\Bearing'
 
-# # 1. 이미지 크롤링
-# for keyword in keywords:
-#     print('Searching...' , keyword[0])
-#     save_folder = os.path.join(raw_image_dic,keyword[1])
-#     print('Save folder directory : ', save_folder)
-#     image_preprocess.crawling_image(keyword=keyword[0]
-#                                     , max_num=100
-#                                     , min_size=100
-#                                     , save_dir=save_folder)
-#
+# 1. 이미지 크롤링
+for keyword in keywords:
+    print('Searching...' , keyword[0])
+    save_folder = os.path.join(raw_image_dic,keyword[1])
+    print('Save folder directory : ', save_folder)
+    image_preprocess.crawling_image(keyword=keyword[0]
+                                    , max_num=100
+                                    , min_size=100
+                                    , save_dir=save_folder)
+
 # 2. 이미지 정제 및 변환
 
-# for keyword in keywords:
-#     endmsg= image_preprocess.change_image(dir = raw_image_dic  ,
-#                                   save_dir = preprocess_image_dic ,
-#                                   keyword = keyword[1],
-#                                   extension='png' ,
-#                                   change_size=(256,256),
-#                                   color=None,
-#                                   delete=0)
-#     print(endmsg)
-#
+for keyword in keywords:
+    endmsg= image_preprocess.change_image(dir = raw_image_dic  ,
+                                  save_dir = preprocess_image_dic ,
+                                  keyword = keyword[1],
+                                  extension='png' ,
+                                  change_size=(256,256),
+                                  color=None,
+                                  delete=0)
+    print(endmsg)
+
 # 3. 이미지 폴더 병합
-#
-# image_preprocess.image_folder_merge(img_dir=preprocess_image_dic,
-#                    merge_dir=merge_dir ,
-#                    merge_keyword='bearing')
+
+image_preprocess.image_folder_merge(img_dir=preprocess_image_dic,
+                   merge_dir=merge_dir ,
+                   merge_keyword='bearing')
 
 
 # 4. fake 이미지 생성
-# img_num= 1
-# image_preprocess.generator_fake_image(img_num=1,
-#                                       img_dir=merge_dir,
-#                                       save_dir=merge_dir,
-#                                       prefix_keyword='Bearing',
-#                                       save_extension='png')
-#
+img_num= 1
+image_preprocess.generator_fake_image(img_num=1,
+                                      img_dir=merge_dir,
+                                      save_dir=merge_dir,
+                                      prefix_keyword='Bearing',
+                                      save_extension='png')
+
 
 # 4. fake 이미지로 데이터 채우기
 image_preprocess.fill_fake_image(img_dir=preprocess_image_dic, img_num=100 , chagne_num=1)
--- Data/select_serarch.py
+++ Data/select_serarch.py
@@ -3,7 +3,7 @@
 # # reading the image
 import cv2
 # image = cv2.imread("example.jpg")
-image = cv2.imread("C:\\Users\\HyunA\\PycharmProjects\\CNN_Deeplearning\\Data\\Dataset\\Product\\Preprocess\\test\\result2.png")
+image = cv2.imread("C:\\Users\\HyunA\\PycharmProjects\\CNN_Deeplearning\\Data\\Dataset\\Product\\Preprocess\\test\\test_test4.jpeg")
 edged = cv2.Canny(image, 10, 250)
 cv2.imshow("Edges", edged)
 cv2.waitKey(0)
@@ -28,19 +28,20 @@ cv2.waitKey(0)
 
 
 # import cv2
-image = cv2.imread("C:\\Users\\HyunA\\PycharmProjects\\CNN_Deeplearning\\Data\\Dataset\\Product\\Preprocess\\test\\result2.png")
+image = cv2.imread("C:\\Users\\HyunA\\PycharmProjects\\CNN_Deeplearning\\Data\\Dataset\\Product\\Preprocess\\test\\test_test4.jpeg")
 # gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 edged = cv2.Canny(image, 10, 250)
 (_,cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 idx = 0
 for c in cnts:
 	x,y,w,h = cv2.boundingRect(c)
-	if w>300 and h>300:
+	if w>400 and h>400:
 		idx+=1
 		new_img=image[y:y+h,x:x+w]
 		cv2.imwrite(str(idx) + '.png', new_img)
 		cv2.imshow("im", new_img)
 		cv2.waitKey(0)
-#
-# cv2.imshow("im",new_img)
-# cv2.waitKey(0)
+
+# #
+# # cv2.imshow("im",new_img)
+# # cv2.waitKey(0)
